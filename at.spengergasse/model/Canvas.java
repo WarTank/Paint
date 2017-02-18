5c3130afbad74dce159df47ad17afef039c69346
Raphael Herdlicka <raphael.herdlicka@gmail.com>
2017-02-18 18:18:00 +0100

    Added basic javaFX

--- at.spengergasse/model/Canvas.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Canvas {
-	private Pixel[][] raster;
-
-	
-}
--- at.spengergasse/model/Pixel.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Pixel {
-	public int r,g,b;
-	public Pixel() {
-		// TODO Auto-generated constructor stub
-	}
-}
