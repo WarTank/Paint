dbce54ad92c770b503d68d257bd1bf6915e7d349
Raphael <raphael.herdlicka@gmail.com>
2017-03-10 14:00:43 +0100

    Added missing FXML

--- at/spengergasse/application/ImageTest.java
+++ at/spengergasse/application/ImageTest.java
@@ -4,18 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import spengergasse.model.MousePositionThread;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
-import javafx.scene.paint.Color;
 import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import spengergasse.model.MousePositionThread;
 
 /**
  * Created by HER17491 on 10.03.2017.
