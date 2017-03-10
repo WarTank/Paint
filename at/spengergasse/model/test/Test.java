8ad50b5bf2a197a0df3ed0cd31d94c003e7cfb21
Peter <pete.wendlinger@hotmail.com>
2017-03-10 14:06:31 +0100

    Added import option in test.Main

--- at/spengergasse/model/test/Test.java
+++ at/spengergasse/model/test/Test.java
@@ -1,11 +1,5 @@
 package spengergasse.model.test;
 
-import spengergasse.model.Canvas;
-
 public class Test {
-	static Canvas canvas;
-	
-	public static void main(String[] args) {
-		canvas = new Canvas(5,3);
-	}
+
 }
